f3d60de0f360730e09c14cf42b68a0f2e0014c10
Baha <ferchichibaha7@gmail.com>
2019-11-05 10:49:23 +0100

    add User Profile add User Summary add Username/email Check Availability

--- src/main/java/io/baha/fstgate/controllers/UserController.java
+++ src/main/java/io/baha/fstgate/controllers/UserController.java
@@ -1,6 +1,9 @@
 package io.baha.fstgate.controllers;
 import io.baha.fstgate.exception.ResourceNotFoundException;
 import io.baha.fstgate.message.ApiResponse;
+import io.baha.fstgate.message.UserIdentityAvailability;
+import io.baha.fstgate.message.UserProfile;
+import io.baha.fstgate.message.UserSummary;
 import io.baha.fstgate.models.Post;
 import io.baha.fstgate.models.Role;
 import io.baha.fstgate.models.RoleName;
@@ -28,9 +31,34 @@ public class UserController {
     @Autowired
 private RoleRepository roleRepository;
 
-    @GetMapping("/me")
-    public UserPrincipal getme(@CurrentUser UserPrincipal currentUser) {
-        return currentUser;
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
+        return userSummary;
+    }
+
+    @GetMapping("/user/checkUsernameAvailability")
+    public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
+        Boolean isAvailable = !userRepository.existsByUsername(username);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/user/checkEmailAvailability")
+    public UserIdentityAvailability checkEmailAvailability(@RequestParam(value = "email") String email) {
+        Boolean isAvailable = !userRepository.existsByEmail(email);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/users/{username}")
+    public UserProfile getUserProfile(@PathVariable(value = "username") String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+
+
+        UserProfile userProfile = new UserProfile(user.getId(), user.getUsername(), user.getName(), user.getCreatedAt());
+
+        return userProfile;
     }
 
 
